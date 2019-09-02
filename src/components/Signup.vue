39348cadc6bee0c5c2ba1bf975c06e26fe1c9c9f
Matthew Lee <peanutmaster@Matthews-MacBook-Pro-3.local>
2019-09-02 19:08:04 +0800

    updates

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,25 +1,73 @@
 <template>
     <div>
-        <div>
-        <input v-model="email" placeholder="Enter your email">
-        <input v-model="username" placeholder="Enter your username">
-        <input v-model="password" type="password" placeholder="Enter your password">
-        </div>
-        <button @click="submit" v-on:enter="submit" href="/login">submit</button>
-
+      <div>
+        <br><h1>Sign Up</h1>
+        <el-form>
+         <p>Email</p>
+          <el-form-item>
+        <el-input style="max-width : 300px" v-model="email" placeholder="Enter your email"/>
+           </el-form-item>
+          <p>Username</p>
+          <el-form-item>
+        <el-input style="max-width : 300px" v-model="username" placeholder="Enter your username"/>
+          </el-form-item>
+          <p>Password</p>
+          <el-form-item>
+        <el-input style="max-width : 300px" v-model="password" type="password" placeholder="Enter your password" />
+          </el-form-item>
+          <p>Cover Photo</p>
+          <el-form-item>
+          <file-pond
+        name="image"
+                class="imageBox"
+        label-idle="Select/drop files here..."
+        allow-multiple="true"
+        accepted-file-types="image/jpeg, image/png"
+        v-bind:files="file"
+        server="https://63ecca8f.ap.ngrok.io/upload"
+        :onprocessfile="uploadCover"
+     />        
+     </el-form-item>
+     <p>Avatar</p>
+       <el-form-item>
+ <file-pond
+        class="imageBox"
+        name="image"
+        label-idle="Select/drop files here..."
+        allow-multiple="true"
+        accepted-file-types="image/jpeg, image/png"
+        v-bind:files="file"
+        server="https://63ecca8f.ap.ngrok.io/upload"
+        :onprocessfile="uploadAvatar"
+     />        
+     </el-form-item>
+          <el-form-item>
+        <el-button @click="submit" v-on:enter="Submit" href="/login" primary>submit</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import vueFilePond, { setOptions } from 'vue-filepond';
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'; 
+import { send } from 'q';
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
 
 export default {
   name: 'Signup',
   components :{
-
+    FilePond,
   },
   data() {
     return {
+      file:[],
       username : null,
       email : null,
       password : null,
@@ -57,6 +105,23 @@ export default {
       this.$router.push(`/profile/${this.username}`)
       location.reload()
     },
+     uploadAvatar(err, file) {
+        let image = JSON.parse(file.serverId)
+        console.log(image.url)
+        this.avatar = image.url
+    },
+     uploadCover(err, file) {
+        let image = JSON.parse(file.serverId)
+        this.cover = image.url
+    },
   }
 }
 </script>
+<style scoped lang="scss">
+.imageBox {
+ 
+  width:50%;
+  margin-left: 25%
+}
+
+</style>
